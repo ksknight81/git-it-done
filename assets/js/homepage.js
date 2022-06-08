bf057f130944e13c310389f0b6a8fb30f802c9e2
kevin knight <ksknight81@gmail.com>
2022-06-07 23:54:54 -0400

    updated fetch to include function to pull different users data instead of hardcoded

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,25 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
-};
 
-getUserRepos();
+
+var getUserRepos = function(user) {
+
+    var user = "ksknight81";
+    //format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    //make a request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+
+    // fetch("https://api.github.com/users/octocat/repos").then(function(response){
+    // //this is a nested .then() which calls the specific 'response' data into an array.  Ie, the specific repos
+    // response.json().then(function(data){
+    //     console.log(data);
+    // });
+    // console.log("inside", response);
+    // });
+}
+getUserRepos()
+console.log("outside")
